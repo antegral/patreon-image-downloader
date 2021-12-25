e3e58548d9b65b813bb3812d2ecf933ba54e9e26
antegral <antegral@antegral.net>
2021-12-25 18:00:09 +0900

    Fix constructor Fix variable name Fix metadataScraper

--- src/included.worker.ts
+++ src/included.worker.ts
@@ -1,69 +1,73 @@
-import got from "got";
+import got, { ToughCookieJar } from "got";
 import log from "./logger";
 import { CookieJar } from "tough-cookie";
 import { promisify } from "util";
 import { writeFileSync } from "fs";
 import { join } from "path";
 import {
-  patreonPostPageObject,
   patreonPostIncludeObject,
   patreonDownloadableContent,
 } from "../@types/patreonPost";
 
 export default class includedWorker {
   private metadataPieces: Array<patreonPostIncludeObject.RootObject[]>;
+  private metadata: patreonDownloadableContent[];
   private sessionId: string;
-  private postDownloaderCnt: number;
-  private setCookie;
+  private downloadMetadataCnt: number;
+  private cookieJar: ToughCookieJar;
 
-  constructor() {
+  constructor(sessionId: string) {
     let cookieJar = new CookieJar();
-    this.setCookie = promisify(cookieJar.setCookie.bind(cookieJar));
-  }
-
-  async downloadMetadata(url: string) {
-    let cookieJar = await this.setCookie(
+    let setCookie = promisify(cookieJar.setCookie.bind(cookieJar));
+    this.sessionId = sessionId;
+    this.metadataPieces = [];
+    setCookie(
       `session_id=${this.sessionId}; Path=/; Domain=.patreon.com; Expires=Sat, 12 Feb 2050 15:53:14 GMT;`,
       "https://www.patreon.com/"
     );
+    this.cookieJar = cookieJar;
+    this.downloadMetadataCnt = 1; // Reset Counter
+  }
 
+  async downloadMetadata(url: string) {
     log(
-      `DEBUG > postDownloader > Downloading Included List... (${this
-        .postDownloaderCnt++})`
+      `downloadMetadata >> Downloading Included List... (${this
+        .downloadMetadataCnt++})`
     );
 
     let { body } = await got(url, {
-      cookieJar,
+      cookieJar: this.cookieJar,
+    }).catch((reason) => {
+      log("downloadMetadata >> UNEXPECTED ERROR!");
+      throw new Error(reason);
     });
 
     if (!JSON.parse(body).links.next) {
       // Unavailable Next Link => End of Data
-      this.postDownloaderCnt = 1; // Reset Counter
-      let metadata = await this.metadataScraper(); // call metadataScraper
-      return metadata; // return converted metadatas.
+      this.downloadMetadataCnt = 1; // Reset Counter
+      this.metadata = await this.metadataScraper(); // call metadataScraper
+      return null; // return converted metadatas.
     } else if (typeof JSON.parse(body).links.next === "string") {
       // Available Next Link
-      this.metadataPieces.push(JSON.parse(body).included); // petchIncluded에 post에서 추출된 included 파일을 push
-      this.downloadMetadata(`https://${JSON.parse(body).links.next}`); // 데이터에 있는 다음 링크로 재귀식 함수 호출
+      this.metadataPieces.push(JSON.parse(body).included); // Parsed된 included 객체를 this.metadataPieces에 push
+      await this.downloadMetadata(`https://${JSON.parse(body).links.next}`); // 데이터에 있는 다음 링크로 재귀식 함수 호출
+      return this.metadata; // return converted metadatas.
     } else {
       // Unexpected Link Parse Error
-      throw new Error("postDownloader >> links.next is not string!");
+      throw new Error("downloadMetadata >> body.links.next is not string!");
     }
   }
 
   async metadataScraper() {
-    let mergedMetadatas: patreonPostIncludeObject.RootObject[];
+    let mergedMetadatas = this.metadataPieces.flat();
     let convedMetadatas: patreonDownloadableContent[];
+    convedMetadatas = [];
 
-    if (!this.metadataPieces) {
+    if (this.metadataPieces.length === 0) {
       throw new Error(
-        "metadataScraper >> metadataPieces is undefined. please check your CLIENT_SESSION_ID."
+        "metadataScraper >> metadataPieces not found. please check your CLIENT_SESSION_ID."
       );
     }
-
-    this.metadataPieces.forEach((e) => {
-      mergedMetadatas.concat(e);
-    });
 
     // include 객체 type별 imageURL Location (imageWorker)
     // media => e.attributes.download_url (Viewer에서 보여지는 원본 사진?)
@@ -77,16 +81,23 @@ export default class includedWorker {
             downloadURL: e.attributes.url,
             type: "attachment",
           });
+          break;
         case "media":
           convedMetadatas.push({
-            filename: e.attributes.name,
+            filename: e.attributes.file_name,
             downloadURL: e.attributes.download_url,
             type: "media",
           });
+          break;
         default:
-          log("non-detectable or unexceptable type. ignored.");
+          log(
+            "metadataScraper >> non-detectable or unexceptable type. ignored."
+          );
+          break;
       }
     }
+
+    return convedMetadatas;
   }
 
   saveMetadata(data: patreonDownloadableContent[]) {
